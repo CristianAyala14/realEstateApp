9bfc8e0dd83e8e53a669054f2ee4f120c3be7e85
Cristian <134509867+CristianAyala14@users.noreply.github.com>
2024-11-04 23:24:49 -0300

    lunes

--- client/src/pages/userListings.jsx
+++ client/src/pages/userListings.jsx
@@ -10,7 +10,7 @@ import "swiper/css/bundle";
 
 export default function UserListings() { 
     //slider
-    SwiperCore.use({Navigation});
+    SwiperCore.use([Navigation]);
 
     const params = useParams();
     const [loading, setLoading] = useState(false)
@@ -24,8 +24,8 @@ export default function UserListings() {
                 setLoading(true)
                 const res = await getListingReq(listingId);
                 if(res.status===200){
-                const listing = res.listing
-                setListing(listing)
+                const objListing = res.listing
+                setListing(objListing)
                 setLoading(false)
                 SetError(false)
 
@@ -46,14 +46,31 @@ export default function UserListings() {
             {loading && <p className='text-center my-7 text-2x1'>Loading... </p>}
             {error && <p className='text-center my-7 text-2x1'>Something went wrong! </p>}
             {listing && !loading && !error && (
+                <>
                 <Swiper navigation>
                     {listing.imageUrls.map((url)=>(
                         <SwiperSlide key={url}>
-                            <div className='h-[550px]' style={{background:`url(${url}) center no-repeat`, backgroundSize: "cover"}}></div>
+                            <div className='h-[550px]' style={{background:`url(${url}) center no-repeat`, backgroundSize: "cover"}}></div>   
                         </SwiperSlide>
-                    ))}
+                    ))} 
                 </Swiper>
+                <h1 className='m-3'>{listing.name} {listing.offer? listing.discountPrice : listing.regularPrice } {listing.type === "rent" && " /month"}</h1>
+                <p className='text-green-700 m-3'>{listing.address}</p>
+                <div className='flex gap-4'> 
+                    <p className='bg-red-900 w-full max-w-[200px] text-white text-center p-1 m-3 rounded-md'>
+                        {listing.type==="rent"? "For rent":"For sale"}
+                    </p>
+                    {listing.offer && (
+                        <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 m-3 rounded-md'>${listing.regularPrice - listing.discountPrice} Less Offer</p>
+                    )}
+                </div>
+
+                <p className='text-slate-800 p-1 m-3'><span className='font-semibold text-black '>Description - </span>
+                    {listing.description}</p>
+                    
+                </>
             )}
+
         </div>
     )
 }
